b98776ff82007b61397e541dbe3ea47796f7dd4f
miltondw <estradamilton2001@gmail.com>
2021-05-21 18:22:52 -0500

    structure from file database and rrouter

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 
 //Settings
-app.set('port',process.env.PORT||3000)
+app.set("port", process.env.PORT || 3000);
 
 //Middlewares
-app.use(express.json())//
+app.use(express.json()); // Previously it was used Body-parser
 
 //Routes
+app.use(require("./routes/employees"));
 
 //Starting the server
-app.listen(app.get('port'), () => {
-  console.log("server on port ",app.get('port'));
+app.listen(app.get("port"), () => {
+  console.log("server on port ", app.get("port"));
 });
